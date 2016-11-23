22aa06e4db5f214555152f75c6c40288a3d5ac02
g9308370 <g9308370@hotmail.com>
2016-11-23 11:32:51 +0800

    rebuild factor

---
+++ src/components/example.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        I'm an example component!
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Eample Component ready.')
+        }
+    }
+</script>
